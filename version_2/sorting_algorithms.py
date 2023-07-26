2d07762c959c75721c3faa53a2f86f7cbec67dff
htsago <132760827+htsago@users.noreply.github.com>
2023-07-26 07:31:15 +0200

    update, new alorithm

--- version_2/sorting_algorithms.py
+++ version_2/sorting_algorithms.py
@@ -24,7 +24,7 @@ input_file ="D:/Sensorprobe/version_2/sanding.csv"
 output_file = 'sanding.csv'
 column_order = ['timestamp','R1_1','R1_2','R1_3','R1_4','R1_5','R1_6','R1_7','R1_8','R1_9','R1_10','R1_11','R1_12','R2_1','R2_2','R2_3','R2_4','R2_5','R2_6','R2_7','R2_8','R2_9','R2_10','R2_11','R2_12','R3_1','R3_2','R3_3','R3_4','R3_5','R3_6','R3_7','R3_8','R3_9','R3_10','R3_11','R3_12','R4_1','R4_2','R4_3','R4_4','R4_5','R4_6','R4_7','R4_8','R4_9','R4_10','R4_11','R4_12','Label','Class']
 
-columns_to_delete = ["b'Port", "b'Port.1", "b'Port.2", "b'Port.3", "b'Port", 'port', "b'Port.1", "b'Port.3", "b'Port.2"]
+columns_to_delete = ["b'Port", "b'Port.1", "b'Port.2", "b'Port.3", "b'Port", 'port', "b'Port.1", "b'Port.3", "b'Port.2", "R2_1","R2_2","R2_3","R2_4","R2_5","R2_6","R2_7","R2_8","R2_9","R2_10","R2_11","R2_12", "R1_12","R3_12","R4_12","R1_2","R1_4","R1_6","R1_8","R1_10","R3_2","R3_4","R3_6","R3_8","R3_10","R4_2","R4_4","R4_6","R4_8","R4_10","R3_1","R3_2","R3_3","R3_4","R3_5","R3_6","R3_7","R3_8","R3_9","R3_10","R3_11"]
 
 swap_columns(input_file, output_file, column_order, columns_to_delete)
 
