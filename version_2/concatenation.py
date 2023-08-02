1d76aabdb41d8b025edccf3fdc0cf692272f2e08
htsago <132760827+htsago@users.noreply.github.com>
2023-08-02 09:53:31 +0200

    update

--- version_2/concatenation.py
+++ version_2/concatenation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 
-csv_files = glob.glob('*.csv')
+csv_files = glob.glob('D:/Sensorprobe/Test/*.csv')
 combined_data = pd.DataFrame()
 
 for file in csv_files:
@@ -9,6 +9,6 @@ for file in csv_files:
     combined_data = pd.concat([combined_data, data])
 
 combined_data.sort_values(by='R1_1', inplace=False)
-combined_data.to_csv('final_data.csv', index=False)
+combined_data.to_csv('painting_01.csv', index=False)
 
 print("Done!")
