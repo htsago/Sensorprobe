ed96b99dd0049d9cb7e16196201c39b3e7e6147d
htsago <132760827+htsago@users.noreply.github.com>
2023-08-14 21:22:17 +0200

    update

---
+++ version_3/concatenation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import glob
+
+csv_files = glob.glob('D:/Sensorprobe/version_3/Test/*.csv')
+combined_data = pd.DataFrame()
+
+for file in csv_files:
+    data = pd.read_csv(file)
+    combined_data = pd.concat([combined_data, data])
+
+combined_data.sort_values(by='R1_1', inplace=False)
+combined_data.to_csv('model_new_01.csv', index=False)
+
+print("Done!")
