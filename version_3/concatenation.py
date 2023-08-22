44da3f48d81e7bf7ebce00c1b893d266fc2fd643
htsago <132760827+htsago@users.noreply.github.com>
2023-08-22 11:57:22 +0200

    update

--- version_3/concatenation.py
+++ version_3/concatenation.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import glob
 
-csv_files = glob.glob('D:/Sensorprobe/version_3/Test/*.csv')
+#csv_files = glob.glob('D:/Sensorprobe/version_3/*.csv')
+csv_files = glob.glob('D:/Sensorprobe/version_3/Test1/*.csv')
 combined_data = pd.DataFrame()
 
 for file in csv_files:
@@ -9,6 +10,6 @@ for file in csv_files:
     combined_data = pd.concat([combined_data, data])
 
 combined_data.sort_values(by='R1_1', inplace=False)
-combined_data.to_csv('model_new_01.csv', index=False)
+combined_data.to_csv('model_new_02.csv', index=False)
 
 print("Done!")
